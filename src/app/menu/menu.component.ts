a5ddd6ac8494174f8c064d151a22cf1f8dbf4569
just-me- <hess-marcel@bluewin.ch>
2023-12-03 22:46:50 +0100

    fixing some smaller ios display issues

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core'
+import {Component, ElementRef, inject, OnDestroy, OnInit, ViewChild} from '@angular/core'
+import { Subject } from 'rxjs'
+import {NavigationEnd, Router} from '@angular/router'
+import {takeUntil} from 'rxjs/operators'
 
 @Component({
   selector: 'wuza-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit, OnDestroy {
+  @ViewChild('navbarToggler') navbarToggler!: ElementRef
+
+  private unsubscribe$ = new Subject<void>()
+  private router = inject(Router)
+
+  private get navBarToggleIsVisible() {
+    return this.navbarToggler.nativeElement.getAttribute('aria-expanded') === 'true'
+  }
 
   menuEntries = [
     { name: 'Home', path: 'home', icon: 'home'},
@@ -15,5 +26,24 @@ export class MenuComponent {
     // { name: 'Kontakt', path: 'contact', icon: 'face'},
     { name: 'Impressum', path: 'impressum', icon: 'account_balance'},
   ]
+
+  ngOnInit() {
+    // close mobile navigation
+    this.router.events.pipe(takeUntil(this.unsubscribe$)).subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.collapseNav()
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next()
+  }
+
+  private collapseNav() {
+    if (this.navBarToggleIsVisible) {
+      this.navbarToggler.nativeElement.click()
+    }
+  }
 
 }
