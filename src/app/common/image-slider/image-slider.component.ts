d3d8ca1ce2b048285b8276117c0cb7cb40f25016
just-me- <hess-marcel@bluewin.ch>
2023-12-05 22:41:30 +0100

    fix swiper - add pagination

--- src/app/common/image-slider/image-slider.component.ts
+++ src/app/common/image-slider/image-slider.component.ts
@@ -1,27 +1,36 @@
-import {Component, Input} from '@angular/core'
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core'
 import {ImageSliderImage} from './image-slider-image'
+import {SwiperContainer} from 'swiper/swiper-element'
+import {SwiperOptions} from 'swiper/types'
 
 @Component({
   selector: 'wuza-image-slider',
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.scss']
 })
-export class ImageSliderComponent {
+export class ImageSliderComponent implements AfterViewInit {
+  @ViewChild('swiper') swiper!: ElementRef<SwiperContainer>
+
   @Input() images?: ImageSliderImage[]
 
-  swiperConfig: any /*SwiperOptions*/ = {
+  swiperConfig: SwiperOptions = {
     initialSlide: 0,
     speed: 500,
     autoplay: {
+      disableOnInteraction: false,
       delay: 3500
     },
     pagination: {
       type: 'bullets',
-      clickable: 'true'
+      clickable: true
     },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
     }
+  }
+
+  ngAfterViewInit() {
+    this.swiper.nativeElement.swiper.activeIndex = 0
   }
 }
--- src/app/common/image-slider/image-slider.module.ts
+++ src/app/common/image-slider/image-slider.module.ts
@@ -2,11 +2,12 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ImageSliderComponent } from './image-slider.component'
 import { register } from 'swiper/element/bundle'
+import {SwiperDirective} from './swiper.directive'
 
 register()
 
 @NgModule({
-  declarations: [ImageSliderComponent],
+  declarations: [ImageSliderComponent, SwiperDirective],
   exports: [
     ImageSliderComponent
   ],
